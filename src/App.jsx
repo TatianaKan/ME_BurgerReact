61097cb3f13c0da23238fe89c4f606f9eb4b53e2
TatianaKan <klanukt@gmail.com>
2023-02-22 15:32:34 +0300

    Redux Category

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
+import { Provider } from "react-redux"
 import { Catalog } from "./Components/Catalog/CAtalog"
 import { Header } from "./Components/Header/Header"
 import { Navigation } from "./Components/Navigation/Navigation"
+import { store } from "./store"
 
 export const App = () => {
 
   return (
-    <>
+    <Provider store={store}>
+   
       <header><Header /> </header>
 
       <main>
@@ -15,7 +18,7 @@ export const App = () => {
       </main>
 
       <footer></footer>
-    </>
+    </Provider>
   )
 }
 
