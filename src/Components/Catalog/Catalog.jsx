cda532131e4aac4904bbe0762e8288bcddb84596
TatianaKan <klanukt@gmail.com>
2023-02-27 15:20:30 +0300

    Продукты с сервера

--- src/Components/Catalog/Catalog.jsx
+++ src/Components/Catalog/Catalog.jsx
@@ -2,34 +2,35 @@ import style from './Catalog.module.css'
 import { Container } from "../Container/Container"
 import { Order } from "../Order/Order"
 import { CatalogProduct } from '../CatalogProduct/CatalogProduct';
-
-const goodsList = [
-  { title: 'Мясная бомба' },
-  { title: 'Супер сырный' },
-  { title: 'Сытный' },
-  { title: 'Итальянский' },
-  { title: 'Вечная классика' },
-  { title: 'Тяжелый удар' },
-];
-
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { productRequestAsync } from '../../store/product/productSlice';
 
 export const Catalog = () => {
+  const { products } = useSelector(state => state.product);
+  const dispatch = useDispatch();
+  const { category, activeCategory } = useSelector(state => state.category);
+
+  useEffect(() => {
+    if (category.length) {
+      dispatch(productRequestAsync(category[activeCategory].title))
+    }
+  }, [category, activeCategory])
+
   return (
     <section className={style.catalog}>
       <Container className={style.container}>
         <Order />
-
         <div className={style.wrapper}>
-          <h2 className={style.title}>Бургеры</h2>
+          <h2 className={style.title}>{category[activeCategory]?.rus}</h2>
 
           <div className={style.wrap_list}>
             <ul className={style.list}>
-            {goodsList.map((item, i) => (
-              <li key={i} className={style.item}>
-                <CatalogProduct title={item.title}/>
-              </li>
-            ))}
-              
+              {products.map((item) => (
+                <li key={item.id} className={style.item}>
+                  <CatalogProduct item={item} />
+                </li>
+              ))}
             </ul>
           </div>
         </div>
